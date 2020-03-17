ad53cdb6e281127023006d8de588b0a2b27c2dcc
Irishmun <steefiscool70@gmail.com>
2020-03-17 13:33:04 +0100

    put buttoncontroller in seperate js file

---
+++ WebApplication1/wwwroot/js/ButtonSwitcher.js
@@ -0,0 +1,6 @@
+﻿function BecomeSelected(button, ...buttons) {
+    document.getElementById(button).setAttribute("class", "Top-Bar TB-Selected");
+    for (var i = 0; i < buttons.length; i++) {
+        document.getElementById(buttons[i]).setAttribute("class", "Top-Bar TB-Normal");
+    }
+}
