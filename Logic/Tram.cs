2aa7556626cc459eff153bf898ed64d9eaf0c90d
Rick Vrolings <416994@student.fontys.nl>
2020-03-31 15:05:01 +0200

    Deleted proporties from tram logic

--- Logic/Tram.cs
+++ Logic/Tram.cs
@@ -9,28 +9,24 @@ namespace Logic
     public class Tram {
 
         private readonly ITramAccess _tramAccess;
-        public string SerialNumber { get; set; }
-        //public TramType Type { get; set; }
-        public ICollection<TramStatus> Status { get; private set; }
-        private ICollection<Track> PreferredTracksList { get; set; }
 
         public Tram(ITramAccess tramAccess)
         {
-            Status = new List<TramStatus>();
+            //Status = new List<TramStatus>();
             _tramAccess = tramAccess;
         }
 
         public void AddStatus(TramStatus newStatus)
         {
-            if (!Status.Contains(newStatus))
-            {
-                Status.Add(newStatus);
-            }
+            //if (!Status.Contains(newStatus))
+           // {
+           //     Status.Add(newStatus);
+           // }
         }
 
         public void DeleteStatus(TramStatus stat)
         {
-            Status.Remove(stat);
+            //Status.Remove(stat);
 
         }
 
