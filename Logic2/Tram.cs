d118c37d948631070d1500a0c4e6fc017c022500
Rick Vrolings <416994@student.fontys.nl>
2020-04-01 09:59:16 +0200

    Moved Enums to service layer, added service layer.

--- Logic2/Tram.cs
+++ Logic2/Tram.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using DAL.Interfaces;
 using DTO;
-using Logic.Models;
+using Services;
 
 namespace Logic
 {
---
+++ Services/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Services
+{
+    public enum TramStatus
+    {
+        Defect,
+        Cleaning,
+        Shift,
+        Depot
+    }
+    public enum TramType
+    {
+        Combino,
+        DoubleCombino,
+        ElevenG,
+        TwelveG,
+        TrainingTram
+    }
+}
