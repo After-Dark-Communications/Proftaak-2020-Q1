fbdd500ff19f3dc04263b43a55aa2bb09fc12abf
michiel413274 <49024242+michiel413274@users.noreply.github.com>
2020-04-08 11:48:28 +0200

    login repository and logincontroller

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.ShowTopBar = false;
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Repository/LoginRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApplication1.Repository
+{
+    public class LoginRepository
+    {
+        private readonly IHttpContextAccessor contextAccessor;
+
+        public LoginRepository()
+        {
+
+        }
+
+        public LoginRepository(IHttpContextAccessor contextAccessor)
+        {
+            this.contextAccessor = contextAccessor;
+        }
+
+        public void SetLoginSession(string username)
+        {
+            contextAccessor.HttpContext.Session.SetString("UserName", username);
+        }
+
+        public void RemoveLoginSession()
+        {
+            contextAccessor.HttpContext.Session.Remove("Username");
+            contextAccessor.HttpContext.Session.Remove("UserId");
+        }
+    }
+}
