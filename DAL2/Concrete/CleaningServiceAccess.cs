86cda0809e108284e779c4fc3b74065fe92704d7
michiel413274 <49024242+michiel413274@users.noreply.github.com>
2020-04-21 13:48:15 +0200

    cleaning dal

---
+++ DAL2/Concrete/CleaningServiceAccess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Context;
+using DAL.Interfaces;
+using DAL.Models;
+using DTO;
+using Microsoft.Data.SqlClient;
+
+namespace DAL.Concrete
+{
+    public class CleaningServiceAccess : ICleaningServiceAccess
+    {
+        public void Create(CleaningServiceDTO obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CleaningServiceDTO Read(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(CleaningServiceDTO obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CleaningServiceDTO> Logs()
+        {
+            using (SqlConnection conn = new SqlConnection(DBConnection._connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("Select ", conn))
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ DAL2/Interfaces/ICleaningServiceAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Concrete;
+using DTO;
+
+namespace DAL.Interfaces
+{
+    public interface ICleaningServiceAccess : IGenAccess<CleaningServiceDTO>
+    {
+
+    }
+}
