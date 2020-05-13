9e1bbb99337d759fe0b29a30bd9803f09c7482a5
Irishmun <steefiscool70@gmail.com>
2020-05-13 12:02:35 +0200

    added cleaning page and made start of viewmodel

---
+++ WebApplication1/Controllers/ServiceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+
+namespace WebApplication1.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IMapper _mapper;
+        public ServiceController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public IActionResult Repairs()
+        {
+            return View();
+        }
+
+        public IActionResult Cleaning()
+        {
+            return View();
+        }
+    }
+}
